a62ee3fa6b23d9bc28b8383624d42821e1e4e8ab
yudan111 <151390400+yudan111@users.noreply.github.com>
2024-12-19 16:13:33 +0800

    呃呃呃
    呃呃呃

--- src/views/follow.vue
+++ src/views/follow.vue
@@ -1,10 +1,22 @@
 <template>
-  
+    <div>
+         <Nav/>
+     <Follow_comtenter/>
+        <!-- <Footer/> -->
+    </div>
 </template>
 
 <script>
-export default {
+import Follow_comtenter from '@/components/follow_comtenter.vue';
+import Footer from '@/components/footer.vue';
+import Nav from '@/components/nav.vue';
 
+export default {
+  components: {
+    Nav,
+    Follow_comtenter,
+    Footer
+  },
 }
 </script>
 
